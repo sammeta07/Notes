427ec4f3797fe52712ffe79808c8a6c48008c2dc
hari-prasad <hariprasad8976@gmail.com>
2023-08-09 17:48:04 +0530

    typescript basics

---
+++ typescript/code-setp-by-step/index.js
@@ -0,0 +1,10 @@
+var App = /** @class */ (function () {
+    function App() {
+    }
+    App.prototype.test = function () {
+        console.log('Welcome');
+    };
+    return App;
+}());
+var a = new App();
+a.test();
---
+++ typescript/code-setp-by-step/index.ts
@@ -0,0 +1,9 @@
+class App{
+  test(){
+    console.log('Welcome');
+  }
+}
+let a = new App();
+a.test();
+
+
