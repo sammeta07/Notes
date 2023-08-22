a1f3c6245888e483cd77b7d4b6e44ebd41a487af
hari-prasad <hariprasad8976@gmail.com>
2023-08-22 19:57:53 +0530

    js objects

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,12 @@
-let colors = ['red', 'green', 'blue'];
-let rgb = [...colors];
-console.log(rgb);
+let car = {
+  brand: 'Honda',
+  getBrand: function () {
+      return this.brand;
+  }
+}
 
-const circle = {
-  radius: 10
-};
-const coloredCircle = {
-  ...circle,
-  color: 'black'
-};
-console.log(coloredCircle);
+// console.log(car.getBrand()); // Honda
+
+let brand = car.getBrand;
+console.log(brand()); // undefined
+
